6b1b42a94f06339f6e1f4b13adaf61d07ec1125d
40158958 <jmcnamee07@qub.ac.uk>
2026-09-24 21:24:34 +0100

    Changed OOP classes, Player now a dataclass

---
+++ src/data/player.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass
+class Player:
+        firstname: str
+        lastname: str
+        overall: int
+        position: str
+        position_d: str
+        attack: int
+        defense: int
+        ability: int
+        physicality: int
+        tier: str
+        wage: int
+        era: str
+        
